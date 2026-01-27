12aaffb7ab8250ce3608e9c2d0c9c8df6a4ef179
adavilaferdez-code <adavilaferdez@gmail.com>
2026-01-27 21:10:02 +0100

    Add files via upload

---
+++ limpiar.bat
@@ -0,0 +1,11 @@
+@echo off
+mkdir _PAPELERA 2>nul
+move catalogo.html _PAPELERA
+move catalogo_2025.pdf _PAPELERA
+move catalogo_page1.jpg _PAPELERA
+move catalogo_page2.jpg _PAPELERA
+move deudas_v2.jpg _PAPELERA
+move distribuidores.jpg _PAPELERA
+move focos_dashboard.jpg _PAPELERA
+echo Archivos movidos a _PAPELERA. Puede borrarlos definitivamente cuando quiera.
+pause
